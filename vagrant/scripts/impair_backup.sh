d3d21d7156789d870eb7b2bfa621ea76c765a547
akshshar <akshshar@cisco.com>
2016-07-18 09:46:54 -0700

    Fixed the impairment scripts with some outputs

--- vagrant/scripts/impair_backup.sh
+++ vagrant/scripts/impair_backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+echo "Stopping all current impairments"
 sudo tc qdisc del dev eth4 root &> /dev/null
 sudo tc qdisc del dev eth3 root &> /dev/null
 echo "Starting packet loss on backup link"
-sudo tc qdisc add dev eth4 root netem loss 4%
+sudo tc qdisc add dev eth4 root netem loss 7%
