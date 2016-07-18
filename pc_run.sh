130d62db37f0ab00503bdbc0ff43054b9a69053a
akshshar <akshshar@cisco.com>
2016-07-18 09:36:36 -0700

    Added a higher jitter threshold

--- pc_run.sh
+++ pc_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./pathchecker.py --host 6.6.6.6 -u vagrant -p vagrant --port 830 -c 10 -o apphost -a 0 -i GigabitEthernet0/0/0/0 -s 2.2.2.2  -j 4 -l 5 -f -t 10
+./pathchecker.py --host 6.6.6.6 -u vagrant -p vagrant --port 830 -c 10 -o apphost -a 0 -i GigabitEthernet0/0/0/0 -s 2.2.2.2  -j 8 -l 5 -f -t 10
